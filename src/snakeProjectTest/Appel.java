3184d945bd67e0ccc63efa3c6bdd83e43937aa56
Bego Jukic <bego.jukic@gmx.at>
2023-04-01 00:54:18 +0200

    New

---
+++ src/snakeProjectTest/Appel.java
@@ -0,0 +1,5 @@
+package snakeProjectTest;
+
+public class Appel{
+
+}
---
+++ src/snakeProjectTest/testt.java
@@ -0,0 +1,7 @@
+package snakeProjectTest;
+
+public class testt {
+    public static void main(String[] args) {
+        System.out.println("\uF000");
+    }
+}
