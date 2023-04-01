4a65949d0211ce10c66df5272ec475feb074c182
Bego Jukic <bego.jukic@gmx.at>
2023-04-02 01:33:02 +0200

    versuch die grenze anzupassen

--- src/snakeProjectTest/Appel.java
+++ src/snakeProjectTest/Appel.java
@@ -23,7 +23,7 @@ public class Appel{
 
     public boolean appelLocationInConfliftWithSnake(Point[] tryLocation,int widht,int height){
         for (int i = 0, j = 0; i < tryLocation.length ; i++) {
-            if (!tryLocation[i].equals(this.appelLocation) && j < tryLocation.length && ((this.appelLocation.x >= 10 && this.appelLocation.x <= widht-10) && this.appelLocation.y >= 10 && this.appelLocation.y <= widht-10 )) {
+            if ((!tryLocation[i].equals(this.appelLocation) && j < tryLocation.length) && ((appelLocation.x >= 30 && this.appelLocation.x <= widht-30) && (this.appelLocation.y >= 30 && this.appelLocation.y <= height-30) )) {
                 j++;
             }
             else if (tryLocation[i].equals(this.appelLocation)) {
